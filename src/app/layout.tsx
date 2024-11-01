b4bb6fe1865979473f998480e06d951c12318604
vinrain <143264723+vinrain-coder@users.noreply.github.com>
2024-11-01 23:10:56 +0000

    Project and IDE Setup(create-next-app, shadcn UI,Prettier, tailwind plugin, VS Code extensions)

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Lora } from "next/font/google";
+import "./globals.css";
+
+const lora = Lora({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Shoepedi",
+    absolute: "Shoepedi",
+  },
+  description: "Elegant | Stylish | Comfortable Shoes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={lora.className}>{children}</body>
+    </html>
+  );
+}
