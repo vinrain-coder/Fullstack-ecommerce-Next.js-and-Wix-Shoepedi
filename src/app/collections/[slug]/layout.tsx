0248bf18b7c744338941e1700fad916cfef0eacd
vinrain-coder <vincentombogo57@gmail.com>
2024-11-09 09:26:40 +0000

    fix layout

--- src/app/collections/[slug]/layout.tsx
+++ src/app/collections/[slug]/layout.tsx
@@ -1,76 +1,69 @@
+import Product from "@/src/components/Product";
 import { Skeleton } from "@/src/components/ui/skeleton";
-import WixImage from "@/src/components/WixImage";
-import { cn, delay } from "@/src/lib/utils";
 import { getWixServerClient } from "@/src/lib/wix-client.server";
 import { getCollectionBySlug } from "@/src/wix-api/collections";
+import { queryProducts } from "@/src/wix-api/products";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
-interface LayoutProps {
-  children: React.ReactNode;
-  params: { slug?: string } | any;
-}
+// Optional: Enable dynamic rendering
+export const dynamic = 'force-dynamic';
 
-export default function Layout({ children, params }: LayoutProps) {
-  return (
-    <Suspense fallback={<LoadingSkeleton />}>
-      <CollectionsLayout params={params}>{children}</CollectionsLayout>
-    </Suspense>
-  );
-}
-
-async function CollectionsLayout({ children, params: { slug } }: LayoutProps) {
-  await delay(2000);
-
-  const collection = await getCollectionBySlug(getWixServerClient(), slug);
+export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+  const collection = await getCollectionBySlug(getWixServerClient(), params.slug);
 
   if (!collection) notFound();
 
   const banner = collection.media?.mainMedia?.image;
 
+  return {
+    title: collection.name,
+    description: collection.description,
+    openGraph: {
+      images: banner ? [{ url: banner.url }] : [],
+    },
+  };
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const collection = await getCollectionBySlug(getWixServerClient(), params.slug);
+
+  if (!collection?._id) notFound();
+
   return (
-    <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
-      <div className="flex flex-col gap-10">
-        {banner && (
-          <div className="relative hidden sm:block">
-            <WixImage
-              mediaIdentifier={banner.url}
-              width={1280}
-              height={400}
-              alt={banner.altText}
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black" />
-            <h1 className="absolute bottom-10 left-1/2 -translate-x-1/2 text-4xl font-bold text-white lg:text-5xl">
-              {collection.name}
-            </h1>
-          </div>
-        )}
-        <h1
-          className={cn(
-            "mx-auto text-3xl font-bold md:text-4xl",
-            banner && "sm:hidden",
-          )}
-        >
-          {collection.name}
-        </h1>
-      </div>
-      {children}
-    </main>
+    <div className="space-y-5">
+      <h2 className="text-2xl font-bold">Products</h2>
+      <Suspense fallback={<LoadingSkeleton />}>
+        {/* Use server component */}
+        <Products collectionId={collection._id} />
+      </Suspense>
+    </div>
   );
 }
 
+const Products = async ({ collectionId }: { collectionId: string }) => {
+  const collectionProducts = await queryProducts(getWixServerClient(), {
+    collectionIds: collectionId,
+  });
+
+  if (!collectionProducts.length) notFound();
+
+  return (
+    <div className="flex grid-cols-2 flex-col gap-5 sm:grid md:grid-cols-3 lg:grid-cols-4">
+      {collectionProducts.items.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+  );
+};
+
 function LoadingSkeleton() {
   return (
-    <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
-      <Skeleton className="mx-auto h-10 w-48 sm:block sm:aspect-[1280/400] sm:h-full sm:w-full" />
-      <div className="space-y-5">
-        <h2 className="text-2xl font-bold">Products</h2>
-        <div className="flex grid-cols-2 flex-col gap-5 sm:grid md:grid-cols-3 lg:grid-cols-4">
-          {Array.from({ length: 8 }).map((_, i) => (
-            <Skeleton key={i} className="h-[26rem] w-full" />
-          ))}
-        </div>
-      </div>
-    </main>
+    <div className="flex grid-cols-2 flex-col gap-5 sm:grid md:grid-cols-3 lg:grid-cols-4">
+      {Array.from({ length: 8 }).map((_, i) => (
+        <Skeleton key={i} className="h-[26rem] w-full" />
+      ))}
+    </div>
   );
 }
