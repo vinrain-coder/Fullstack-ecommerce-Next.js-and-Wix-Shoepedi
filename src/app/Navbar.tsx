c425fbcc4508f66b700643f32912fa91a9dc9304
vinrain-coder <143264723+vinrain-coder@users.noreply.github.com>
2024-11-11 14:44:44 +0000

    made navigation menu responsive

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -9,6 +9,8 @@ import MainNavigation from "./MainNavigation";
 import UserButton from "@/components/UserButton";
 import ShoppingCartButton from "./ShoppingCartButton";
 import SearchField from "@/components/SearchField";
+import MobileMenu from "./MobileMenu";
+import { Suspense } from "react";
 
 export default async function Navbar() {
   const wixClient = getWixServerClient();
@@ -22,16 +24,28 @@ export default async function Navbar() {
   return (
     <header className="bg-background shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-5 p-5">
+        <Suspense>
+          <MobileMenu
+            collections={collections}
+            loggedInMember={loggedInMember}
+          />
+        </Suspense>
         <div className="flex flex-wrap items-center gap-5">
           <Link href="/" className="flex items-center gap-4">
             <Image src={logo} alt="Shoepedi Logo" width={40} height={40} />
-            <span className="text-3xl font-bold">Shoepedi</span>
+            <span className="text-2xl font-bold">Shoepedi</span>
           </Link>
-          <MainNavigation collections={collections} />
+          <MainNavigation
+            collections={collections}
+            className="hidden lg:flex"
+          />
         </div>
-        <SearchField className="max-w-96"/>
+        <SearchField className="hidden max-w-96 lg:inline" />
         <div className="flex items-center justify-center gap-5">
-          <UserButton loggedInMember={loggedInMember} />
+          <UserButton
+            loggedInMember={loggedInMember}
+            className="hidden lg:inline-flex"
+          />
           <ShoppingCartButton initialData={cart} />
         </div>
       </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { products } from "@wix/stores";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../../tailwind.config";
+
+export const twConfig = resolveConfig(tailwindConfig);
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,23 +16,23 @@ export async function delay(ms: number) {
 
 export function formatCurrency(
   price: number | string = 0,
-  currency: string = "KSH"
+  currency: string = "KSH",
 ) {
   return Intl.NumberFormat("en", { style: "currency", currency }).format(
-    Number(price)
+    Number(price),
   );
 }
 
 export function findVariant(
   product: products.Product,
-  selectedOptions: Record<string, string>
+  selectedOptions: Record<string, string>,
 ) {
   if (!product.manageVariants) return null;
 
   return (
     product.variants?.find((variant) => {
       return Object.entries(selectedOptions).every(
-        ([key, value]) => variant.choices?.[key] === value
+        ([key, value]) => variant.choices?.[key] === value,
       );
     }) || null
   );
@@ -36,7 +40,7 @@ export function findVariant(
 
 export function checkInStock(
   product: products.Product,
-  selectedOptions: Record<string, string>
+  selectedOptions: Record<string, string>,
 ) {
   const variant = findVariant(product, selectedOptions);
 
